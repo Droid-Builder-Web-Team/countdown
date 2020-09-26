25da2d4bdbb181b36a8b0a75141cf2e50533bb3e
Rob Howdle <droidbuilderwebteam@gmail.com>
2020-09-26 13:27:21 +0100

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Vue logo" class="inline img-fluid" src="./assets/Baddeley2020.png" style="width:300px; height:300px;">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+
+    <img alt="Vue logo" class="inline img-fluid" src="./assets/dbwtlogo.png" style="width:300px; height:400px;">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './views/Home.vue'
 
 export default {
   name: 'App',
@@ -22,7 +25,10 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #FFF;
+  height:100vh;
+  width:100%;
+  background: linear-gradient(180deg, #101430 0%, black 100%);
+
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <Counter
+        :year="2020"
+        :month="10"
+        :date="1"
+        :hour="23"
+        :minute="59"
+        :second="59"
+        :millisecond="31"
+        />
+    </div>
+</template>
+
+<script>
+import Counter from '@/components/Counter.vue'
+    export default {
+        components: {
+            Counter
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
